bb0256591bbd130eb9955f00689209dbe6af5e3f
alokchaturvedi44 <chaturvedialok44@gmail.com>
2026-01-23 23:00:20 +0530

    Added Owner, Author, Listing and Review Validation

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
     //  console.log("isLoggedIn hit:", req.session.redirectUrl);
     if(!req.isAuthenticated()){
@@ -11,6 +16,48 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.saveRedirectUrl = (req, res, next) => {
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+}
+
+module.exports.isOwner = async (req, res, next) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error", "you don't have permission to edit");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next) => {
+        let {error} = listingSchema.validate(req.body);
+        if(error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }
+        else{
+            next();
+        }
+}
+
+module.exports.validateReview = (req, res, next) => {
+        let {error} = reviewSchema.validate(req.body);
+        if(error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }
+        else{
+            next();
+        }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error", "You can not delete this review!");
+        return res.redirect(`/listings/${id}`); 
     }
     next();
 }
